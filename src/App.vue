e94f553442b0f3d3d029fe2f3de739b9a22a0922
KellerMarika <marikakeller92@gmail.com>
2022-12-04 11:18:17 +0100

    add img to pub

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
+<template lang="">
+
+<main>
+  <div class="container">
+
+  </div>
+</main>
+
+</template>
+
 <script>
+import TheHeader from './components/TheHeader.vue';
+import TheFooter from './components/TheFooter.vue';
+
 export default {
-  
+
+  components: {
+    TheHeader,
+    TheFooter,
+  },
+
+  data() {
+    return {}
+  },
+
 }
 </script>
-
-<template lang="">
-  <div>
-    
-  </div>
-</template>
 
 <style lang="">
   
